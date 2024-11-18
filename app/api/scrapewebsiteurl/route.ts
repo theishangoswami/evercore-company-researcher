71754167f529758def3254d18f40d91a37517e6c
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-18 23:29:12 +0530

    added url scrape and subpages scrape APIs (exa)

--- app/api/scrapewebsiteurl/route.ts
+++ app/api/scrapewebsiteurl/route.ts
@@ -1,4 +1,4 @@
-// app/api/findcompetitors/route.ts
+// app/api/scrapewebsiteurl/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import Exa from "exa-js";
 
@@ -11,19 +11,12 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'Website URL is required' }, { status: 400 });
     }
 
-    // Use Exa to search for content related to the claim
-    const result = await exa.findSimilarAndContents(
-      websiteurl,
-      {
-        text: true,
-        summary: {
-            query: `Explain in one/two lines what does this company do in simple english. Don't use any diffcult words.`
-          },
-        numResults: 6,
-        livecrawl: "always",
-        excludeDomains: [websiteurl]
-      }
-    );
+    const result = await exa.getContents(
+        [websiteurl],
+        {
+          text: true
+        }
+      );
 
     return NextResponse.json({ results: result.results });
   } catch (error) {
